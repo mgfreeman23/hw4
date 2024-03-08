36654ac31b90deaa4281eaaf1aa20635440d961b
Malina Freeman <mgfreema@usc.edu>
2024-03-08 20:18:22 +0000

    add equal paths

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -1,6 +1,5 @@
 #ifndef RECCHECK
 //if you want to add any #includes like <iostream> you must do them here (before the next endif)
-
 #endif
 
 #include "equal-paths.h"
@@ -9,10 +8,42 @@ using namespace std;
 
 // You may add any prototypes of helper functions here
 
+// helper function to get leaf node height
+bool leafHeight(Node* curr, int height, int& sharedHeight);
 
+// equalPaths function
 bool equalPaths(Node * root)
 {
-    // Add your code below
+    // initialize leaf node height to dummy value
+    int sharedHeight = -1;
+    // call helper function
+    return leafHeight(root, 0, sharedHeight);
+}
 
+bool leafHeight(Node* curr, int height, int& sharedHeight){
+  // if empty subtree, return true
+  if(curr == nullptr){
+    return true;
+  }
+  // if at a leaf node
+  if(curr->left == nullptr && curr->right == nullptr){
+    // initialize the leaf node height to height
+    if(sharedHeight == -1){
+      sharedHeight = height;
+      return true;
+    } 
+    // check if leaf node at same height as other leaf nodes
+    else {
+      return sharedHeight == height;
+    }
+  } 
+  // if not a leaf node, iterate to left and right subtrees
+  // and update height
+  else {
+    return leafHeight(curr->right, height + 1, sharedHeight) 
+    && leafHeight(curr->left, height + 1, sharedHeight);
+
+  }
+  
 }
 
